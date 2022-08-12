9f0643784cdedfef401af23e15502cfa593d9d15
“Sanjar-Khushvaktov” <khushvaktov.s@gmail.com>
2022-08-12 14:41:59 +0000

    Adding S3.tf file

---
+++ S3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-first1111test-bucket"
+
+  tags = {
+    Name        = "My firstt11bucket"
+    Environment = "Dev"
+  }
+
+
+versioning {
+    enabled = true
+  }
+}
+
+# It is my Terraform file to push my Github account S3.tf file
