6c1b9e706e232c12964e8ea67ddfa4ab39fc13b1
“Sanjar-Khushvaktov” <khushvaktov.s@gmail.com>
2022-08-13 14:00:44 +0000

    Creating srcipt files

---
+++ ssh/memory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Check our system.
+
+echo "Memory:"
+free -h
+echo "Disk usage:"
+du -h 
+echo "Uptime:"
+uptime
+
+
---
+++ ssh/script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Hello, world!"
+echo "Welcome to my shell script."
+ls
+echo "Thank you, good bye!"
+echo "this is scripts file"
+
